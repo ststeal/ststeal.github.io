5cc590514ff18e1797b8b71279939547ca37d9f5
Ivan Nikulshin <ststealnik@yandex.ru>
2017-02-15 22:54:05 +0300

    city_sort

--- lessons/static/object.js
+++ lessons/static/object.js
@@ -1,14 +1,22 @@
 var submit = function () {
+    if (document.getElementsByTagName('li')[0] !== null) {
+        $('li').remove();
+    }
     objStr = document.getElementById("obj").value;
     newObj = JSON.parse(objStr);
+
+
     $('span').text('▶ Object ' + objStr);
     for (var i in newObj) {
-        var newLi = document.createElement('li');
-        $(newLi).text(i + ": " + newObj[i]);
-        list.appendChild(newLi);
-        $('li').hide();
+        if (newObj.hasOwnProperty(i)) {
+            var newLi = document.createElement('li');
+            $(newLi).text(i + ": " + newObj[i]);
+            list.appendChild(newLi);
+            $('li').hide();
+        }
     }
 }
+
 var showProp = function () {
     $('li').toggle(500);
     if ($('span').text().charAt(0) === '▶') {
