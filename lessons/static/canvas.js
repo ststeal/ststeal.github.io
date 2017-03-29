6fe00be2d5f5b532f8f26449df5f3187592dd482
Ivan Nikulshin <ststealnik@yandex.ru>
2017-03-29 23:29:34 +0300

    debounce + nextGen

--- lessons/static/canvas.js
+++ lessons/static/canvas.js
@@ -51,8 +51,13 @@ function decl(baseClass, props) {
 var mylib = {};
 /**
  * @class Canvas
+ * @desc Класс содержит фигуры canvas и умеют их рисовать
  */
 mylib.Canvas = decl({
+	/**
+	 * @constructor
+	 * @param {Object} opts
+	 */
 	/**
 	 * @constructor
 	 * @param {Object} opts
@@ -61,12 +66,17 @@ mylib.Canvas = decl({
 		this.id = opts.id;
 		this.shapeList = [];
 	},
+	/**
+	 * Возвращает базовые свойства canvas области
+	 * @returns {{id: string}}
+	 */
 	getDefaultOptions: function () {
 		return {
 			id: '1'
 		};
 	},
 	/**
+	 * Добавляет фигуры в список текущего контекста
 	 * @param {...Shape}
 	 */
 	addShape: function () {
@@ -74,21 +84,45 @@ mylib.Canvas = decl({
 			this.shapeList.push(arguments[i]);
 		}
 	},
+	/**
+	 * Возвращает и удаляет фигуру из списка
+	 * @returns {Shape}
+	 */
 	popShapeList: function () {
-		this.shapeList.pop();
+		return this.shapeList.pop();
 	},
+	/**
+	 * Очищает список фигур
+	 */
 	resetShapeList: function () {
 		this.shapeList = [];
 	},
+	/**
+	 * Очищает область canvas
+	 */
 	clearCanvas: function () {
 		this.context.clearRect(0, 0, this.context.width, this.context.height);
 	},
+	/**
+	 * Рисует добавленные фигуры из списка
+	 */
 	render: function () {
 		this.context = (document.getElementById(this.id)).getContext('2d');
+		//(document.getElementById('1')).width = (document.body).clientWidth/2;
+		//(document.getElementById('1')).height = (document.body).clientHeight/2;
+		//(document.getElementById('2')).width = (document.body).clientWidth/2;
+		//(document.getElementById('2')).height = (document.body).clientHeight/2;
+		//(document.getElementById('3')).width = (document.body).clientWidth/2;
+		//(document.getElementById('3')).height = (document.body).clientHeight/2;
+		//(document.getElementById('4')).width = (document.body).clientWidth/2;
+		//(document.getElementById('4')).height = (document.body).clientHeight/2;
 		for (var i = 0; i < this.shapeList.length; i++) {
 			this.shapeList[i].render(this.context);
 		}
 	},
+	/**
+	 * Вывод фигур, добавленных в список
+	 */
 	shapes: function () {
 		for (var i = 0; i < this.shapeList.length; i++) {
 			console.log(this.shapeList[i]);
@@ -97,11 +131,14 @@ mylib.Canvas = decl({
 });
 /**
  * @class Shape
+ * @lends Shape
+ * @alias mylib.Shape
+ * Базовая фигура
  */
 mylib.Shape = decl({
 	$constructor: function () {
 	},
-	render: function () {
+	render: function (context) {
 		console.log('render method');
 	},
 	toString: function () {
@@ -110,8 +147,17 @@ mylib.Shape = decl({
 });
 /**
  * @class Rect
+ * @extends Shape
+ * @lends Rect
+ * @alias mylib.Rect
+ * Прямоугольная фигура
+ *
+ * @example
+ * <code>
+ *      var rect1 = new mylib.Rect({color: 'blue', x: 0, y: 50, width: 50, height: 50});
+ * </code>
  */
-mylib.Rect = decl(mylib.Shape,{
+mylib.Rect = decl(mylib.Shape, {
 	/**
 	 * @constructor
 	 * @param {Object} opts
@@ -123,6 +169,10 @@ mylib.Rect = decl(mylib.Shape,{
 		this.width = opts.width;
 		this.height = opts.height;
 	},
+	/**
+	 * Возвращает базовые свойтва прямоуголника
+	 * @returns {{color: string, x: number, y: number, width: number, height: number}}
+	 */
 	getDefaultOptions: function () {
 		return {
 			color: 'green',
@@ -133,19 +183,27 @@ mylib.Rect = decl(mylib.Shape,{
 		};
 	},
 	/**
-	 *
-	 * @param {Object} context
+	 * Рисует прямоугольник
+	 * @param {CanvasRenderingContext2D} context
 	 */
 	render: function (context) {
-		//mylib.Shape.super.render.apply(this, arguments);
 		context.fillStyle = this.color;
 		context.fillRect(this.x, this.y, this.width, this.height);
 	}
 });
 /**
  * @class Circle
+ * @extends Shape
+ * @lends Circle
+ * @alias mylib.Circle
+ * Окружность
+ *
+ * @example
+ * <code>
+ *     var circle2 = new mylib.Circle({cx: 50, cy: 50, r: 40, color: 'blue'});
+ * </code>
  */
-mylib.Circle = decl({
+mylib.Circle = decl(mylib.Shape, {
 	/**
 	 * @constructor
 	 * @param {Object} opts
@@ -156,6 +214,10 @@ mylib.Circle = decl({
 		this.r = opts.r;
 		this.color = opts.color;
 	},
+	/**
+	 * Возвращает базовые свойтва окружности
+	 * @returns {{cx: number, cy: number, r: number, color: string}}
+	 */
 	getDefaultOptions: function () {
 		return {
 			cx: 50,
@@ -165,11 +227,10 @@ mylib.Circle = decl({
 		};
 	},
 	/**
-	 *
-	 * @param {Object} context
+	 * Рисует окружность
+	 * @param {CanvasRenderingContext2D} context
 	 */
 	render: function (context) {
-		//mylib.Shape.super.render.apply(this, arguments);
 		context.beginPath();
 		context.arc(this.cx, this.cy, this.r, 0, 2 * Math.PI);
 		context.lineWidth = 5;
@@ -180,6 +241,14 @@ mylib.Circle = decl({
 /**
  * @class LabeledCircle
  * @extends Circle
+ * @lends LabeledCircle
+ * @alias mylib.labeledCircle
+ * Окружность с заголовком
+ *
+ * @example
+ * <code>
+ *      var labeledCircle1 = new mylib.LabeledCircle({cx: 100, cy: 100, text: 'Первый', textColor: 'blue', color: 'red'});
+ * </code>
  */
 mylib.LabeledCircle = decl(mylib.Circle, {
 	/**
@@ -187,13 +256,14 @@ mylib.LabeledCircle = decl(mylib.Circle, {
 	 * @param {Object} opts
 	 */
 	$constructor: function (opts) {
-		this.cx = opts.cx;
-		this.cy = opts.cy;
-		this.r = opts.r;
 		this.text = opts.text;
 		this.textColor = opts.textColor;
 		this.color = opts.color;
 	},
+	/**
+	 * Возвращает базовые свойства окружности с заголовком
+	 * @returns {{cx: number, cy: number, r: number, text: string, textColor: string, color: string}}
+	 */
 	getDefaultOptions: function () {
 		return {
 			cx: 50,
@@ -205,8 +275,8 @@ mylib.LabeledCircle = decl(mylib.Circle, {
 		};
 	},
 	/**
-	 *
-	 * @param {Object} context
+	 * Рисует окружность с заголовком
+	 * @param {CanvasRenderingContext2D} context
 	 */
 	render: function (context) {
 		mylib.LabeledCircle.super.render.apply(this, arguments);
