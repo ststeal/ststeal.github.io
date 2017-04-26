783f02ea47d27a725dbbd2a9cc60c18190467f3b
Ivan Nikulshin <ststealnik@yandex.ru>
2017-04-27 00:40:48 +0300

    static temoplate task remaster

--- lessons/static_template_task/main.js
+++ lessons/static_template_task/main.js
@@ -1,92 +1,43 @@
 /* globals doT $*/
 var template = doT.template('<div>Hi {{=it.title}}!</div><div>{{=it.body || "This body is empty"}}' + '</div>');
-var myDiv = document.createElement('div');
-var xhr_one = 1;
-var xhr_two = 2;
-var xhr_three = 3;
+var res = {};
+var lastReq;
 
-function listener(xhr) {
-	return xhr.onreadystatechange = function () {
-		if (this.readyState === 4) {
-			try {
-				this.res = template(JSON.parse(xhr.responseText));
-				insertContent(this.res);
-			}
-			catch (error) {
-				console.log('request aborted');
-			}
-		}
-	};
-}
 
-function insertContent(res) {
-	myDiv.innerHTML = res;
-	$('.content_box').append(myDiv);
-}
-
-function abort() {
-	for (var i = 0; i < arguments.length; i++) {
-		if (arguments[i].readyState === 1) {
-			arguments[i].abort();
-		}
+function abortReq() {
+	if (lastReq) {
+		lastReq.abort();
 	}
 }
 
-//
-// function ddd(xhr, source, tab) {
-// 	if (event.target.id === tab) {
-// 		if (!xhr.responseText) {
-// 			xhr = new XMLHttpRequest();
-// 			listener(xhr);
-// 			xhr.open('GET', source, true);
-// 			xhr.send();
-// 		}
-// 		else {
-// 			insertContent(xhr.res);
-// 		}
-// 	}
-// }
+
+function req(target) {
+	abortReq();
+	var newResKey = target.attr('id') + '_req';
+	var newReqKey = target.attr('id') + '';
+	res[newResKey] = $.ajax({
+		url: target.data('src'),
+		success: function (data) {
+			res[newReqKey] = template(JSON.parse(data));
+			$('.content_box').html(res[newReqKey]);
+		}
+	});
+	lastReq = res[newResKey];
+}
+
 
 $('.menu_item').on('click', function (event) {
-
-	if ($('.menu_item_active')[0] !== event.target) {
+	var $target = $(event.target);
+	if (!$target.hasClass('menu_item_active')) {
 		$('.menu_item_active').removeClass('menu_item_active');
-		event.target.classList.add('menu_item_active');
-		if (event.target.id === 'one') {
-			if (!xhr_one.responseText) {
-				xhr_one = new XMLHttpRequest();
-				listener(xhr_one);
-				xhr_one.open('GET', 'source1.json', true);
-				xhr_one.send();
-				abort(xhr_three, xhr_two);
-			}
-			else {
-				insertContent(xhr_one.res);
-			}
+		$target.addClass('menu_item_active');
+		var eventRes = res[$target.attr('id')];
+		if (!eventRes) {
+			req($target);
 		}
-		if (event.target.id === 'two') {
-			if (!xhr_two.responseText) {
-				xhr_two = new XMLHttpRequest();
-				listener(xhr_two);
-				xhr_two.open('GET', 'source2.json', true);
-				xhr_two.send();
-				abort(xhr_one, xhr_three);
-			}
-			else {
-				insertContent(xhr_two.res);
-			}
-		}
-		if (event.target.id === 'three') {
-			if (!xhr_three.responseText) {
-				xhr_three = new XMLHttpRequest();
-				listener(xhr_three);
-				xhr_three.open('GET', 'source3.json', true);
-				xhr_three.send();
-				abort(xhr_two, xhr_one);
-			}
-			else {
-				insertContent(xhr_three.res);
-			}
+		else {
+			$('.content_box').html(eventRes);
+			abortReq();
 		}
 	}
 });
