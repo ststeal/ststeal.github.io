54231a26eae5b053f69d6e7ef496c1698dd1c9eb
Ivan Nikulshin <ststealnik@yandex.ru>
2017-06-05 20:40:51 +0300

    fix position

--- lessons/liquid_column/index.js
+++ lessons/liquid_column/index.js
@@ -1,5 +1,6 @@
 var resizer = document.querySelector('.resize');
 var container = document.querySelector('.container');
+var containerPositionX = container.offsetLeft;
 var first = document.querySelector('.first');
 var second = document.querySelector('.second');
 var minColumnWidth = 200;
@@ -16,18 +17,19 @@ window.addEventListener('mouseup', function () {
 
 function mousemove(event) {
 	var firstWidth = first.offsetWidth;
-	if (event.screenX < minColumnWidth || event.screenX > maxColumnWidth) {
-		if (event.screenX < minColumnWidth) {
+	var newWidth = event.screenX - containerPositionX;
+	if (newWidth < minColumnWidth || newWidth > maxColumnWidth) {
+		if (newWidth < minColumnWidth) {
 			first.style.width = minColumnWidth + 'px';
 			second.style.width = maxColumnWidth + 'px';
 		}
-		if (event.screenX > maxColumnWidth) {
+		if (newWidth > maxColumnWidth) {
 			first.style.width = maxColumnWidth + 'px';
 			second.style.width = minColumnWidth + 'px';
 		}
 	}
 	else {
-		first.style.width = event.screenX + 'px';
+		first.style.width = newWidth + 'px';
 		second.style.width = containerWidth - firstWidth + 'px';
 	}
 }
