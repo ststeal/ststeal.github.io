8e11ad19172b438ead741829d1e982d1f4c0d6df
Ivan Nikulshin <ststealnik@yandex.ru>
2017-09-20 22:54:35 +0300

    cube 2.0

--- lessons/3d_cube/index.js
+++ lessons/3d_cube/index.js
@@ -1,6 +1,13 @@
 var cube = document.querySelector('.container'),
-	range = document.querySelector('.rotation');
-range.addEventListener('input', function () {
-	console.log(range.value);
-	cube.style.transform = ['rotateX(',range.value,'deg)'].join('');
+	rotation = document.querySelector('.rotation'),
+	distance = document.querySelector('.distance');
+
+rotation.addEventListener('input', function () {
+	console.log(rotation.value);
+	cube.style.transform = ['rotateX(',rotation.value,'deg)'].join('');
+});
+
+distance.addEventListener('input', function () {
+	console.log(distance.value);
+	cube.style.transform = ['translateZ(',distance.value,'px)'].join('');
 });
