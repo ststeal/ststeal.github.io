504bdc21c8879237853252e0d9a98c920710ec99
Ivan Nikulshin <ststealnik@yandex.ru>
2017-10-02 12:25:02 +0300

    some cubes fix

--- lessons/3d_cube_2/index.js
+++ lessons/3d_cube_2/index.js
@@ -2,11 +2,14 @@ var cube = document.querySelector('.cube'),
 	preCube = document.querySelector('.pre-cube'),
 	rotationInput = document.querySelector('.rotation'),
 	distanceInput = document.querySelector('.distance'),
-	rotationValue,distanceValue;
+	rotationValue= rotationInput.value,
+	distanceValue;
+
+cube.style.transform = ['rotateX(', rotationValue, 'deg) rotateY(20deg)'].join('');
 
 rotationInput.addEventListener('input', function () {
 	rotationValue = rotationInput.value;
-	cube.style.transform = ['rotateX(', rotationValue, 'deg)'].join('');
+	cube.style.transform = ['rotateX(', rotationValue, 'deg) rotateY(20deg)'].join('');
 });
 
 distanceInput.addEventListener('input', function () {
--- lessons/3d_cube_3/index.js
+++ lessons/3d_cube_3/index.js
@@ -5,8 +5,10 @@ var cube = document.querySelector('.cube'),
 
 speedInput.addEventListener('input', function () {
 	speedValue = speedInput.value;
-	preCube.style.animation = ['translateZ ',speedValue,'s ease-in infinite both'].join((''));
-	cube.style.animation = ['rotateX ',speedValue,'s ease-in infinite both'].join('');
-	preCube.style.animationDirection = 'alternate';
-	cube.style.animationDirection = 'alternate';
+	// preCube.style.animation = ['translateZ ',speedValue,'s ease-in infinite both'].join((''));
+	// cube.style.animation = ['rotateX ',speedValue,'s ease-in infinite both'].join('');
+	preCube.style.animationDuration = [speedValue,'s'].join((''));
+	cube.style.animationDuration = [speedValue,'s'].join('');
+	// preCube.style.animationDirection = 'alternate';
+	// cube.style.animationDirection = 'alternate';
 });
