25b029f8a5f5c73f7b5f4d2a28284a06bda8aa7b
Ivan Nikulshin <ststealnik@yandex.ru>
2017-10-26 20:47:34 +0300

    some fix

--- lessons/adaptive_menu/index.js
+++ lessons/adaptive_menu/index.js
@@ -1,19 +1,23 @@
 var menu = document.querySelector('.menu');
 
-
 document.querySelector('.menu-button').addEventListener('click', function () {
 	menu.classList.toggle('menu-bar');
-
 });
 
 document.querySelector('.overlay').addEventListener('click', function () {
-	menu.classList.toggle('menu-bar');
+	menu.classList.toggle('menu-bar', false);
 });
 
-window.addEventListener('resize', function () {
-	if (document.querySelector('.menu-bar')) {
-		if (window.matchMedia('(min-width: 420px)').matches) {
-			menu.classList.remove('menu-bar');
-		}
+// window.addEventListener('resize', function () {
+// 	if (menu.classList.contains('.menu-bar')) {
+// 		if (window.matchMedia('(min-width: 420px)').matches) {
+// 			menu.classList.toggle('menu-bar',false);
+// 		}
+// 	}
+// });
+
+window.matchMedia('(min-width: 420px)').addEventListener('change', function (event) {
+	if (event.matches) {
+		menu.classList.remove('menu-bar');
 	}
 });
