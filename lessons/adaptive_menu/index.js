4212ba22f4d6dad88cc664348b21ad3038e4f580
Ivan Nikulshin <ststealnik@yandex.ru>
2017-10-26 22:33:30 +0300

    add rem and aria-status

--- lessons/adaptive_menu/index.js
+++ lessons/adaptive_menu/index.js
@@ -1,10 +1,20 @@
 var menu = document.querySelector('.menu');
+var overlay = document.querySelector('.overlay');
+var menuButton = document.querySelector('.menu-button');
 
-document.querySelector('.menu-button').addEventListener('click', function () {
+if (window.innerWidth<420){
+	menu.setAttribute('aria-expanded', 'false');
+}
+
+menuButton.addEventListener('click', function () {
 	menu.classList.toggle('menu-bar');
+	var pressed = (menuButton.getAttribute('aria-pressed') === 'true');
+	var show = (menu.classList.contains('menu-bar') === true);
+	menu.setAttribute('aria-expanded', String(show));
+	menuButton.setAttribute('aria-pressed', String(!pressed));
 });
 
-document.querySelector('.overlay').addEventListener('click', function () {
+overlay.addEventListener('click', function () {
 	menu.classList.toggle('menu-bar', false);
 });
 
@@ -19,5 +29,12 @@ document.querySelector('.overlay').addEventListener('click', function () {
 window.matchMedia('(min-width: 420px)').addEventListener('change', function (event) {
 	if (event.matches) {
 		menu.classList.remove('menu-bar');
+		menu.removeAttribute('aria-expanded');
+	}
+});
+
+window.matchMedia('(max-width: 420px)').addEventListener('change', function (event) {
+	if (event.matches) {
+		menu.setAttribute('aria-expanded', 'false');
 	}
 });
